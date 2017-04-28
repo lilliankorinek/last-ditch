160ff58b67cac3275c5059a2755f22f53d44e544
Alexander Zimmer <Alexander@172-16-235-129.DYNAPOOL.NYU.EDU>
2017-04-28 19:15:16 -0400

    fully functional button pressing

--- assets/js/tap.js
+++ assets/js/tap.js
@@ -3,13 +3,15 @@ var myElement = document.getElementById('doubleTap');
 // We create a manager object, which is the same as Hammer(), but without the presetted recognizers. 
 var mc = new Hammer.Manager(myElement);
 
+var press = 0;
+var timeout;
 
 // Tap recognizer with minimal 2 taps
 mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
 // Single tap recognizer
 mc.add( new Hammer.Tap({ event: 'singletap' }) );
 // add press option
-mc.add ( new Hammer.Press({ event: 'press'}) );
+mc.add ( new Hammer.Press({ event: 'press', time: 2000}) );
 
 
 // we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
@@ -19,9 +21,36 @@ mc.get('singletap').requireFailure('doubletap');
 //press test
 mc.get('press').requireFailure('singletap');
 
-
-
-mc.on("singletap doubletap press", function(ev) {
+mc.on("singletap doubletap", function(ev) {
     console.log(ev.type);
-    console.log(ev);
 });
+
+mc.on("press", function(ev) {
+    console.log(ev.type);
+//    console.log(press);
+    pressSuccess();
+    if (press == 2) {
+    console.log ("the shit hit the fan");
+}
+});
+
+
+
+
+
+    
+function pressSuccess (){
+    console.log ("let's go!");
+    press++
+    console.log(press);
+    if (press == 1) {
+    timeout = setTimeout(function(){ press = 0; console.log(press)}, 6000);
+        }
+    else if (press == 2) {
+        {
+            clearTimeout(timeout);
+            console.log ("timeoutCleared");
+            
+        }
+    }
+}
