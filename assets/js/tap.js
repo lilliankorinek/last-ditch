de7309f6aa2d8287b640e374ec726ca977dbb4f7
Alexander Zimmer <Alexander@172-16-235-129.DYNAPOOL.NYU.EDU>
2017-04-28 17:37:37 -0400

    press control added

--- assets/js/tap.js
+++ assets/js/tap.js
@@ -8,15 +8,20 @@ var mc = new Hammer.Manager(myElement);
 mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
 // Single tap recognizer
 mc.add( new Hammer.Tap({ event: 'singletap' }) );
+// add press option
+mc.add ( new Hammer.Press({ event: 'press'}) );
 
 
 // we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
 mc.get('doubletap').recognizeWith('singletap');
 // we only want to trigger a tap, when we don't have detected a doubletap
 mc.get('singletap').requireFailure('doubletap');
+//press test
+mc.get('press').requireFailure('singletap');
 
 
 
-mc.on("singletap doubletap", function(ev) {
+mc.on("singletap doubletap press", function(ev) {
     console.log(ev.type);
+    console.log(ev);
 });
